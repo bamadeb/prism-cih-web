abd699793970f8d5b141951a0fbfb65c99776fc9
rpsahu <rudraprasadsahu@evidant.com>
2025-12-08 20:18:24 +0530

    fixex inject userservice

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppEnvService } from './services/app-env.service';
+import { IStorageService, StorageService } from './services/storage.service';
 
 
 export const appConfig: ApplicationConfig = {
@@ -12,6 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     AppEnvService,
+    {
+        provide: IStorageService,
+        useClass: StorageService
+    },
     provideAppInitializer(() => {
       const envService = inject(AppEnvService);
       return envService.load(); // directly returns Promise<void>
