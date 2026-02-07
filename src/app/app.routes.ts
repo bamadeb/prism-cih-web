d1c1aa005f25c522c7c5ccc8e299590b71b6aa19
padmaonly <pradhan.padma@gmail.com>
2026-02-07 11:28:08 +0530

    password expiry

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,7 @@ import { SystemLog } from './views/reports/system-log/system-log';
 import { FileLogReport } from './views/reports/file-log-report/file-log-report';
 import { PcrFile } from './views/process-file/pcr-file/pcr-file';
 import { AppointmentList } from './views/reports/appointment-list/appointment-list/appointment-list';
+import { ChangePassword } from './views/change-password/change-password';
 
 export const routes: Routes = [
   {
@@ -25,6 +26,13 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: Login },
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      { path: 'change-password', component: ChangePassword },
     ]
   },
   {
---
+++ src/app/views/change-password/change-password.ts
@@ -0,0 +1,120 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { Title } from '@angular/platform-browser';
+
+import { ConfigService } from '../../services/api.service';
+import { UserDataService } from '../../services/user-data-service';
+
+@Component({
+  selector: 'app-change-password',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatIconModule
+  ],
+  templateUrl: './change-password.html',
+  styleUrl: './change-password.css',
+})
+export class ChangePassword {
+
+  newPassword = '';
+  confirmPassword = '';
+  isLoading = false;
+  errorMessage = '';
+  currentYear: number = new Date().getFullYear();
+  bgImages = [
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg'
+  ];
+  currentIndex = 0;
+
+  constructor(
+    private router: Router,
+    private titleService: Title,
+    private apiService: ConfigService,
+    private userData: UserDataService
+  ) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('PRISM :: CHANGE PASSWORD');
+    
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.bgImages.length;
+    }, 4000);
+  }
+
+  async onSubmit(): Promise<void> {
+    this.clearError();
+    const riskObsUpdateArray: any[] = [];
+    if (this.newPassword !== this.confirmPassword) {
+      this.errorMessage = 'Passwords do not match';
+      return;
+    }
+
+    if (this.newPassword.length < 6) {
+      this.errorMessage = 'Password must be at least 6 characters';
+      return;
+    }
+
+    const user = this.userData.getUser();
+    //console.log(user);
+    if (!user) {
+      this.errorMessage = 'Session expired. Please login again.';
+      return;
+    }
+
+    this.isLoading = true;
+
+    try {
+        const cognito_username = user.cognito_username;
+      if (!cognito_username) {
+        throw { code: 'COGNITO_USERNAME_MISSING' };
+      }
+
+      await this.apiService.updateCognitoUser(cognito_username,{},this.newPassword);
+        const apiparamUpdate = {
+          ID: user.ID,
+          Password: this.newPassword,
+          password_last_changed: new Date()
+        };
+        const updatequalitygapresult = await this.apiService.prismUserPasswordUpdate<any>(apiparamUpdate);
+
+     // await this.apiService.updateUser(payload);
+
+      // 3️⃣ Clear user session and force re-login
+      //this.errorMessage = "Password change successfuly.Please login.";
+      this.userData.clearUser();
+      //this.router.navigate(['/login']);
+
+    this.router.navigate(['/login'], {
+      state: {
+        message: 'Password changed successfully. Please login.'
+      }
+    });      
+
+    } catch (error) {
+      console.error('Password update failed', error);
+      this.errorMessage = 'Failed to update password. Please try again.';
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  clearError(): void {
+    this.errorMessage = '';
+  }
+}
